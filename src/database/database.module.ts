f5a0a2fac277fdd376f44d02938f964c19502432
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-09-17 12:41:54 -0300

    feat: add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class DatabaseModule {
+  static forRoot(): DynamicModule {
+    return {
+      global: true,
+      module: DatabaseModule,
+      providers: [PrismaService],
+      exports: [PrismaService],
+    }
+  }
+}
