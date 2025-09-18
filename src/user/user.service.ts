c05a0d53bdb6947b4d514fabbeb985e33a7f4a75
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-09-18 08:15:06 -0300

    feat: add update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,11 +5,11 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) { }
 
   async create(createUserDto: CreateUserDto) {
-    const {email, confirmEmail, password, name } = createUserDto
-    if(confirmEmail !== email) {
+    const { email, confirmEmail, password, name } = createUserDto
+    if (confirmEmail !== email) {
       return "The emails aren`t equals"
     }
     const user = await this.prisma.user.create({
@@ -30,12 +30,31 @@ export class UserService {
         id,
       }
     })
-    if(!user) return "User not exists"
+    if (!user) return "User not exists"
     return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { email, confirmEmail, password, name } = updateUserDto
+    if (confirmEmail !== email) return "The emails aren`t equals"
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+    if (!user) return "User not exists"
+
+    const updateUser = {
+      email, password, name
+    }
+    const updatedUser = this.prisma.user.update({
+      where: {
+        id
+      },
+      data: { ...user, ...updateUser }
+    })
+    return updatedUser;
   }
 
   remove(id: number) {
